7369190b171fda84cd93230a219e708922ae018c
ivan2porta <132475555+ivan2porta@users.noreply.github.com>
2026-01-02 23:56:14 +0100

    Programar el personaje

--- escenas/personaje/personaje.gd
+++ escenas/personaje/personaje.gd
@@ -1,16 +1,39 @@
 extends CharacterBody2D
 
-var platanos: int = 3
-var dinero: float = -2.3
-var saludo: String = "hola"
-var encendido: bool = true
+@export var animacion: AnimatedSprite2D
 
+var _velocidad: float = 100.0
+var _velocidad_salto: float = -300.0
 
-func _ready():
-	mi_funcion()
-
-func mi_funcion():
-	if encendido:
-		print("Lampara encendida")
+func _physics_process(delta: float):
+	animacion.play("correr")
+	
+	# gravedad
+	velocity += get_gravity() * delta
+	
+	# salto
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = _velocidad_salto
+	
+	# movimiento lateral
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = _velocidad
+		animacion.flip_h = true
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -_velocidad
+		animacion.flip_h = false
 	else:
-		print("Lampara apagada")
+		velocity.x = 0
+		
+	# animacion
+	if !is_on_floor():
+		animacion.play("saltar")
+	elif velocity.x != 0:
+		animacion.play("correr")
+	else:
+		animacion.play("idle")
+		
+	
+	move_and_slide()
+	
+	
