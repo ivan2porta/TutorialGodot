2623aaa8882f217edde3eb2ddb552976b85aec50
ivan2porta <132475555+ivan2porta@users.noreply.github.com>
2026-01-03 16:14:59 +0100

    Interfaz

--- escenas/contador_muertes/contador_muertes.gd
+++ escenas/contador_muertes/contador_muertes.gd
@@ -6,4 +6,4 @@ func _ready() -> void:
 	ControladorGlobal.muertes_actualizado.connect(_actualizar_texto)
 	
 func _actualizar_texto():
-	label.text = "Muertes: " + str(ControladorGlobal.muertes)
+	label.text = str(ControladorGlobal.muertes)
